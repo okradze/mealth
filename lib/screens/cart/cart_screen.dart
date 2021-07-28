a49ffb244a72383a587fc98e2cfc891d0faf3502
Mirian <okradzemirian@gmail.com>
2021-07-28 09:13:08 +0400

    cart screen

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_app/models/models.dart';
+import 'package:flutter_ecommerce_app/screens/screens.dart';
 import 'package:flutter_ecommerce_app/widgets/widgets.dart';
 
 class CartScreen extends StatelessWidget {
@@ -16,6 +18,42 @@ class CartScreen extends StatelessWidget {
     return Scaffold(
       appBar: CustomAppBar(title: 'Cart'),
       bottomNavigationBar: CustomNavBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Add \$20.0 for FREE delivery',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.black,
+                    shape: RoundedRectangleBorder(),
+                    elevation: 0,
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(HomeScreen.routeName);
+                  },
+                  child: Text(
+                    'Add More Items',
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline5!
+                        .copyWith(color: Colors.white),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(height: 15),
+            CartProductCard(product: Product.products[0]),
+            CartProductCard(product: Product.products[1]),
+          ],
+        ),
+      ),
     );
   }
 }
